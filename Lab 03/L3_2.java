df2176cf7b7fcd3107ee0aea0fb5a0fd46720dad
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-01-04 17:59:07 +0530

    Create L3_2.java

---
+++ Lab 03/L3_2.java
@@ -0,0 +1,14 @@
+/*
+Write a program to calculate the division obtained by the student.
+1. Write a program to find that given number or string is palindrome or not.
+ */
+
+import java.util.Scanner;
+public class L3_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter NO. or String : ");
+        String s = sc.nextLine();
+        int i;
+    }
+}
