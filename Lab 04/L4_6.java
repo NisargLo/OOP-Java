05e819f33bfc9986abb037ab708bb3b79ce20c65
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-01-29 19:15:23 +0530

    29-1-24

--- Lab 04/L4_6.java
+++ Lab 04/L4_6.java
@@ -3,13 +3,14 @@
 public class L4_6 {
      public static void main(String[] args) {
           String s = args[0];
-          for(byte i=0;i<s.length();i++){
+          for(byte i=0;i<args.length;i++){
                if(args[i].charAt(0)>='A' && args[i].charAt(0)<='Z'){
+                    System.out.print(args[i]+" ");
                     continue;
                }
                else{
-                    System.out.println("Error: Could not run Java Program - L4_6.java");
-                    break;
+                    System.out.println("Error: Could not run Java Program - L4_6.java\nError occured at - \'"+args[i]+"'");
+                    return;
                }
           }
      }
--- Lab 04/L4_8.java
+++ Lab 04/L4_8.java
@@ -8,20 +8,22 @@
      *
 */
 
-public class L4_8 {
+public class n {
      public static void main(String[] args) {
           byte i,j;
-          for(i=1;i<=3;i++) {
-               for(j=0;j<i;j++){
-                    System.out.print("*");
+          for(i=1;i<=5;i++) {
+               if(i<=3){
+                    for(j=0;j<i;j++){
+                         System.out.print("* ");
+                    }
+                    System.out.println(); 
+               }                   
+               else{
+                    for(j=2;j>=(i-3);j--){
+                         System.out.print("* ");
+                    }
+                    System.out.println();
                }
-               System.out.println();                    
-          }
-          for(i=1;i<=2;i++){
-               for(j=2;j>=i;j--){
-                    System.out.print("*");
-               }
-               System.out.println();
           }
      }
 }
