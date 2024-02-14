92c012f5758f36f8c9872da502018f3173cf8024
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-02-14 21:17:46 +0530

    Update L5_7.java

--- Lab 05/L5_7.java
+++ Lab 05/L5_7.java
@@ -3,5 +3,24 @@
 import java.util.Scanner;
 
 public class L5_7 {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nEnter NO. (which is stored in static variable) :");
+        int n=sc.nextInt();
+        Check c1=new Check();
+        c1.thisN1(n);
+        c1.printN();
+        sc.close();
+    }
+}
+
+class Check{
+    static int n;
+    public void thisN1(int n){
+        this.n=n;
+    }
+
+    public void printN(){
+        System.out.println("\nYour No. N = "+n+"\nHence, 'this' keyword can access static variables of class.");
+    }
 }
