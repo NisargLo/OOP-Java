cb2a4ba0fde6ce9ef2225aa99e54b060b3908b6c
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-02-17 22:40:14 +0530

    17-2-24

--- Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_2.java
+++ Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_2.java
@@ -2,28 +2,28 @@
 
 public class M_2 {
      public static void main(String[] args) {
-          double ans=0,p;
-          int i,j,n=Scan_DifferentMethods.n;
-          Scan_DifferentMethods s=new Scan_DifferentMethods();
-          double[] x=new double[n];
-          double[] y=new double[n];
-          double[] a=new double[n];
-          for(i=0;i<n;i++){
-               x[i]=s.x[i];
-               y[i]=s.y[i];
+          double ans = 0, p;
+          int i, j, n = Scan_DifferentMethods.n;
+          Scan_DifferentMethods s = new Scan_DifferentMethods();
+          double[] x = new double[n];
+          double[] y = new double[n];
+          double[] a = new double[n];
+          for (i = 0; i < n; i++) {
+               x[i] = s.x[i];
+               y[i] = s.y[i];
           }
-          p=(s.d-x[0])/(x[1]-x[0]);
-          for(i=0;i<n;i++){
-               a[i]=y[0];
-               for(j=1;j<n;j++){
-                    y[j-1]=y[j]-y[j-1];
+          p = (s.d - x[0]) / (x[1] - x[0]);
+          for (i = 0; i < n; i++) {
+               a[i] = y[0];
+               for (j = 1; j < n; j++) {
+                    y[j - 1] = y[j] - y[j - 1];
                }
           }
-          for(i=1;i<n;i++){
-               ans=ans+s.p_Minus(p,i)*a[i]/s.factorial(i);
+          for (i = 1; i < n; i++) {
+               ans = ans + s.p_Minus(p, i) * a[i] / s.factorial(i);
           }
-          ans+=a[0];
-          System.out.printf("\nAnswer : y(%.2f) = %.5f",s.d,ans);
+          ans += a[0];
+          System.out.printf("\nAnswer : y(%.2f) = %.5f", s.d, ans);
           System.out.println();
      }
 }
--- Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_5.java
+++ Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_5.java
@@ -1,50 +1,49 @@
 // Gauss's Backward Difference Method
 
-public class M_5{
-     public static void main(String[] args){
-          int i,j,t=0,n=Scan_DifferentMethods.n;
-          Scan_DifferentMethods s=new Scan_DifferentMethods();
-          double ans=0,p;
-          double[] x=new double[n];
-          double[] y=new double[n];
-          double[] a=new double[n];
-          for(i=0;i<n;i++){
-               x[i]=s.x[i];
-               y[i]=s.y[i];
+public class M_5 {
+     public static void main(String[] args) {
+          int i, j, t = 0, n = Scan_DifferentMethods.n;
+          Scan_DifferentMethods s = new Scan_DifferentMethods();
+          double ans = 0, p;
+          double[] x = new double[n];
+          double[] y = new double[n];
+          double[] a = new double[n];
+          for (i = 0; i < n; i++) {
+               x[i] = s.x[i];
+               y[i] = s.y[i];
           }
-          for(i=0;i<n;i++){
-               if((x[i]-s.d)>=0 && i>0){
-                    t=i;
+          for (i = 0; i < n; i++) {
+               if ((x[i] - s.d) >= 0 && i > 0) {
+                    t = i;
                     break;
                }
           }
-          p=(s.d-x[t])/(x[1]-x[0]);
-          for(i=0;i<n;i++){
-               if(t<0){
-                    a[i]=0;
-               }
-               else{
-                    a[i]=y[t];
-                    if(i%2==0){
+          p = (s.d - x[t]) / (x[1] - x[0]);
+          for (i = 0; i < n; i++) {
+               if (t < 0) {
+                    a[i] = 0;
+               } else {
+                    a[i] = y[t];
+                    if (i % 2 == 0) {
                          t--;
                     }
-                    for(j=1;j<n;j++){
-                         y[j-1]=y[j]-y[j-1];
+                    for (j = 1; j < n; j++) {
+                         y[j - 1] = y[j] - y[j - 1];
                     }
                }
           }
-          for(i=1;i<n;i++){
-               if(a[i]!=0){
-                    ans=ans+s.p_Plus_Minus(p,i)*a[i]/s.factorial(i);
-               }
-               else{
+          for (i = 1; i < n; i++) {
+               if (a[i] != 0) {
+                    ans = ans + s.p_Plus_Minus(p, i) * a[i] / s.factorial(i);
+                    System.out.println("\nans : " + ans);
+               } else {
                     break;
                }
           }
-          ans+=a[0];
-          System.out.printf("\nAnswer : y(%.2f) = %.5f",s.d,ans);
+          ans += a[0];
+          System.out.printf("\nAnswer : y(%.2f) = %.5f", s.d, ans);
           System.out.println();
-    }
+     }
 }
 
 // By - Nisarg Lo
