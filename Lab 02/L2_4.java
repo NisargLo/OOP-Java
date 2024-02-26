bf8acdb5dc397652d1252decec8b95adb9f20554
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-02-26 23:57:26 +0530

    26-2-24

--- Lab 02/L2_4.java
+++ Lab 02/L2_4.java
@@ -5,7 +5,7 @@ public class L2_4 {
         Scanner sc=new Scanner(System.in);
         System.err.println("Enter Radius of Circle : ");
         int r=sc.nextInt();
-        double a=(3.14)*(r*r);
+        double a=(Math.PI)*(r*r);
         System.err.println("\nArea of Circle = "+a);
         sc.close();
     }
--- Lab 05/L5_1/Circle.java
+++ Lab 05/L5_1/Circle.java
@@ -1,5 +1,7 @@
 //Write a program to create circle class with area function to find area of circle.
 
+package L5_1;
+
 public class Circle {
      int r;
      public double Area(double r){
