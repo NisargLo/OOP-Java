98471b7dcfbc32bb5fa0326dfbb73797737ba46c
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-03-07 23:16:48 +0530

    Update L6_7.java

--- Lab 06/L6_7.java
+++ Lab 06/L6_7.java
@@ -2,6 +2,15 @@
 
 public class L6_7 {
     public static void main(String[] args) {
-        
+        Pie pi=new Pie();
+        System.out.println("\nValue of Pie upto 15 decimal places - "+pi.getPie()+"\nIt is final (fix), & can't be changed.");
+    }
+}
+
+final class Pie{
+    final double value_of_pie = Math.PI;
+
+    final double getPie(){
+        return value_of_pie;
     }
 }
