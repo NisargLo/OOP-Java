853774da5191c3681672437046a9268a512d3ba4
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-03-07 22:51:01 +0530

    7-3-24

--- Lab 06/L6_5.java
+++ Lab 06/L6_5.java
@@ -11,12 +11,18 @@
 
 public class L6_5 {
     public static void main(String[] args) {
-
+        ThreeDPoint tdp0=new ThreeDPoint();
+        ThreeDPoint tdp1=new ThreeDPoint(1,2,3);
+        ThreeDPoint tdp2=new ThreeDPoint(6,7,8);
+        double ans1=tdp1.distance(tdp0);
+        double ans2=tdp2.distance(10,11,12);
+        System.out.println("\nDistance between points ("+tdp1.X+","+tdp1.Y+","+tdp1.getZ()+") & ("+tdp0.X+","+tdp0.Y+","+tdp0.getZ()+") = "+ans1+" Units");
+        System.out.println("\nDistance between points ("+tdp2.X+","+tdp2.Y+","+tdp2.getZ()+") & ("+tdp2.S1+","+tdp2.S2+","+tdp2.S3+") = "+ans2+" Units");
     }
 }
 
 class ThreeDPoint extends MyPoint{
-    int Z;
+    int Z, S3;
 
     ThreeDPoint(){
         X=0;
@@ -28,5 +34,20 @@ class ThreeDPoint extends MyPoint{
         this.X=X;
         this.Y=Y;
         this.Z=Z;
+    }
+
+    public int getZ(){
+        return Z;
+    }
+
+    public double distance(ThreeDPoint obj){
+        return Math.pow( Math.pow(obj.X-this.X,2) + Math.pow(obj.Y-this.Y,2) + Math.pow(obj.Z-this.Z,2) ,0.5);
+    }
+
+    public double distance(int X, int Y, int Z){
+        S1=X;
+        S2=Y;
+        S3=Z;
+        return Math.pow( Math.pow(X-this.X,2) + Math.pow(Y-this.Y,2) + Math.pow(Z-this.Z,2),0.5);
     }
 }
