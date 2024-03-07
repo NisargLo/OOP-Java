576f5e84813d809bffc40c3a924e609a562edb01
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-03-07 23:20:55 +0530

    7-3-24

---
+++ Lab 07/L7_2.java
@@ -0,0 +1,15 @@
+/*
+    Write a program that illustrates interface inheritance.
+    Interface A is extended by A1 and A2.
+    Interface A12 inherits from both P1 and P2.
+    Each interface declares one constant and one method.
+    Class B implements A12.
+    Instantiate B and invoke each of its methods.
+    Each method displays one of the constants
+ */
+
+public class L7_2 {
+    public static void main(String[] args) {
+
+    }
+}
--- Lab 07/Lab_7__1/L7_1_2.java
+++ Lab 07/Lab_7__1/L7_1_2.java
@@ -1,6 +1,10 @@
 /*
     Type - 2 (using toString() method)
-    The abstract vegetable class has three subclasses named Potato, Brinjal and Tomato. Write a java program that demonstrates how to establish this class hierarchy. Declare one instance variable of type String that indicates the color of a vegetable. Create and display instances of these objects. Override the toString() method of object to return a string with the name of vegetable and its color.
+    The abstract vegetable class has three subclasses named Potato, Brinjal and Tomato.
+    Write a java program that demonstrates how to establish this class hierarchy.
+    Declare one instance variable of type String that indicates the color of a vegetable.
+    Create and display instances of these objects.
+    Override the toString() method of object to return a string with the name of vegetable and its color.
  */
 
 package Lab_7__1;
