e759189a5ed8d1d19cdd9b15f9fd3c92ce6f6beb
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-03-08 10:40:53 +0530

    8-3-24

---
+++ Lab 07/L7_3.java
@@ -0,0 +1,13 @@
+/*
+    The Transport interface declares a deliver () method.
+    The abstract class Animal is the super class of the Tiger, Camel, Deer and Donkey classes.
+    The Transport interface is implemented by the Camel and Donkey classes.
+    Write a test program that initialize an array of four Animal objects.
+    If the object implements the Transport interface, the deliver () method is invoked.
+ */
+
+public class L7_3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lab 07/L7_5.java
@@ -0,0 +1,12 @@
+/*
+    The Transport interface declares a deliver () method.
+    The abstract class Animal is the super class of the Tiger, Camel, Deer and Donkey classes.
+    The Transport interface is implemented by the Camel and Donkey classes. Write a test program that initialize an array of four Animal objects.
+    If the object implements the Transport interface, the deliver () method is invoked.
+ */
+
+public class L7_5 {
+    public static void main(String[] args) {
+
+    }
+}
