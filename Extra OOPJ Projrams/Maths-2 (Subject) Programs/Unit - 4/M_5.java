899c5e68b663b8b326fa9c9b0bbcb93bf9c25b30
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2024-03-19 22:25:59 +0530

    19-3-24

--- Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_5.java
+++ Extra OOPJ Projrams/Maths-2 (Subject) Programs/Unit - 4/M_5.java
@@ -19,15 +19,14 @@ public class M_5 {
                }
           }
           p = (s.d - x[t]) / (x[1] - x[0]);
-          for (i = 0; i < n; i++) {
+          for (i = 0, c = 0; i < n; i++) {
                if (t < 0) {
                     a[i] = 0;
                } else {
                     a[i] = y[t];
-                    if (i % 2 == 0) {
+                    if (i % 2 == 0 && i > 0) {
                          t--;
                     }
-                    c = 0;
                     for (j = 1; j < n; j++) {
                          if (j <= n - c) {
                               y[j - 1] = y[j] - y[j - 1];
@@ -35,8 +34,8 @@ public class M_5 {
                               y[j - 1] = 0;
                               y[j] = 0;
                          }
-                         c++;
                     }
+                    c++;
                }
           }
           for (i = 1; i < n; i++) {
